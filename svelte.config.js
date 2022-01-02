d42b97b7dc4e347d2b20f2a659ed77ae1d80a52d
Maxime Péloquin <maxime.peloquin@gmail.com>
2022-01-02 12:26:22 -0500

    feat(deployment): update netlify for sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
     preprocess: [preprocess({})],
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            split: false,
+        }),
 
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
